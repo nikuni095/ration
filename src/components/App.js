55d53e7997009cce99b4b602c4689befda1a38a5
nikuni095 <nikhil.uni095@gmail.com>
2020-04-01 15:39:36 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+class App extends React.Component{
+
+
+    render(){
+        return (
+
+            <div style={{backgroundColor:'aqua'}}>
+            <div>
+                <h1>helllo</h1>
+            </div>
+            </div>
+        )
+
+    }
+
+
+}
+
+export default App
