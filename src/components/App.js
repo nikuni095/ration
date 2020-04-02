28e4bfb60d7030450b4565ee751c1ec8a03fc3bf
nikuni095 <nikhil.uni095@gmail.com>
2020-04-02 14:18:38 +0530

    day2 commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,39 @@
 import React from 'react'
+import List from './List'
+
+const rationDetail=[
+    {name:"aalo",sellingPrice:50,purchasedPrice:44},
+    {name:'payaz',sellingPrice:50,purchasedPrice:44}
+]
 
 class App extends React.Component{
+    state={items:rationDetail}
 
 
     render(){
+        
         return (
-
-            <div style={{backgroundColor:'aqua'}}>
             <div>
-                <h1>helllo</h1>
+            <div className='text-primary'  style={{backgroundColor:'aqua'}}>
+                <h1>Ration List</h1>
+            </div>
+            <div>
+            <div className='text-secondary'>Serach Item{this.state.items.map(listDetail=>{
+                return (
+                    <div className='form-inline'>
+                    <h1>{listDetail.name} </h1>
+                    <h3>{listDetail.sellingPrice}</h3>
+                    <h3>{listDetail.purchasedPrice}</h3>
+                    <button className="
+                    btn btn-danger" >Edit</button>
+                    </div>
+                    
+                )
+            })
+            }
+            </div>
+            
+            <List />
             </div>
             </div>
         )
---
+++ src/components/List.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+class List extends React.Component {
+   state={list:[]}
+
+   render(){
+       return(
+         <h1>helloo
+         </h1>
+       )
+   }
+
+}
+export default List
